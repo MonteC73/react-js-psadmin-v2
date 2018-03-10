639a4a27d7d2c092b6fbed9aa3c51d589fd50339
MonteC73 <jarekucinski@gmail.com>
2018-03-10 20:08:25 +0100

    Create Redux Infrastructure: store, action, reducer

---
+++ src/reducers/courseReducer.js
@@ -0,0 +1,10 @@
+export default function courseReducor(state = [], action) {
+    switch(action.type) {
+        case 'CREATE_COURSE':
+            return [...state, 
+                Object.assign({}, action.course)];
+
+        default:
+            return state;
+    }
+}
