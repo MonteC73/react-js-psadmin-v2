55c4dce3578acff3688fc7d2e7649ef2f06910e7
MonteC73 <jarekucinski@gmail.com>
2018-03-11 22:26:49 +0100

    Create Axaj Preloader Component

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -17,6 +17,13 @@ class ManageCoursePage extends React.Component {
         this.saveCourse = this.saveCourse.bind(this);
     }
 
+    componentWillReceiveProps(nextProps) {
+        if(this.props.course.id != nextProps.course.id) {
+            // Necessary to populate form when existing course is loaded directly.
+            this.setState({course: Object.assign({}, nextProps.course)});
+        }
+    }
+
     updateCourseState(event){
         const field = event.target.name;
         let course = Object.assign({}, this.state.course);
@@ -46,15 +53,27 @@ class ManageCoursePage extends React.Component {
 ManageCoursePage.propTypes = {
     course: PropTypes.object.isRequired,
     authors: PropTypes.array.isRequired,
-    actions: PropTypes.obect.isRequired
+    actions: PropTypes.object.isRequired
 };
 
 ManageCoursePage.contextTypes = {
     router: PropTypes.object
 };
 
+function getCourseById(courses, id) {
+    const course = courses.filter(course => course.id == id);
+    if(course.length) return course[0]; // since filter returns an array
+    return null;
+}
+
 function mapStateToProps(state, ownProps) {
+    const courseId = ownProps.params.id; // from the path `/course/:id`
+
     let course = {id: '', watchHref: '', title: '', authorId: '', length: '', category: ''};
+
+    if(courseId && state.courses.length > 0) {
+        course = getCourseById(state.courses, courseId);
+    }
 
     const authorsFormattedForDropdown = state.authors.map(author => {
         return {
