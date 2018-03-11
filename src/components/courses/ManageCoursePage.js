8abab51bd0fb33aaffefd5cb5d36dd1c4b8d67cf
MonteC73 <jarekucinski@gmail.com>
2018-03-11 21:05:24 +0100

    Add Course feature

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -2,26 +2,70 @@ import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
+import CourseForm from './CourseForm';
 
 class ManageCoursePage extends React.Component {
     constructor(props, context) {
         super(props, context);
+
+        this.state = {
+            course: Object.assign({}, this.props.course),
+            errors: {}
+        };
+
+        this.updateCourseState = this.updateCourseState.bind(this);
+        this.saveCourse = this.saveCourse.bind(this);
+    }
+
+    updateCourseState(event){
+        const field = event.target.name;
+        let course = Object.assign({}, this.state.course);
+        course[field] = event.target.value;
+        return this.setState({course: course});
+    }
+
+    saveCourse(event) {
+        event.preventDefault();
+        this.props.actions.saveCourse(this.state.course);
+        this.context.router.push('/courses');
     }
 
     render() {
         return (
-            <h1>Manage Course</h1>
+            <CourseForm 
+                course={this.state.course}
+                allAuthors={this.props.authors}
+                errors={this.state.errors}
+                onChange={this.updateCourseState}
+                onSave={this.saveCourse}
+                />
         );
     }
 }
 
 ManageCoursePage.propTypes = {
-    //myProp: PropTypes.string.isRequired
+    course: PropTypes.object.isRequired,
+    authors: PropTypes.array.isRequired,
+    actions: PropTypes.obect.isRequired
+};
+
+ManageCoursePage.contextTypes = {
+    router: PropTypes.object
 };
 
 function mapStateToProps(state, ownProps) {
+    let course = {id: '', watchHref: '', title: '', authorId: '', length: '', category: ''};
+
+    const authorsFormattedForDropdown = state.authors.map(author => {
+        return {
+            value: author.id,
+            text: author.firstName + ' ' + author.lastName
+        };
+    });
+
     return {
-        state: state
+        course: course,
+        authors: authorsFormattedForDropdown
     };
 }
 
