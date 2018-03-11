42c2b82695f7357ac668931d60ce2f3119dc8abc
MonteC73 <jarekucinski@gmail.com>
2018-03-11 23:29:37 +0100

    Fix Bug: Update Course

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -6,12 +6,12 @@ export function loadCoursesSuccess(courses) {
     return {type: types.LOAD_COURSES_SUCCESS, courses };
 }
 
-export function createCoursesSuccess(courses) {
-    return {type: types.CREATE_COURSES_SUCCESS, courses };
+export function createCourseSuccess(course) {
+    return {type: types.CREATE_COURSE_SUCCESS, course };
 }
 
-export function updateCoursesSuccess(courses) {
-    return {type: types.UPDATE_COURSES_SUCCESS, courses };
+export function updateCourseSuccess(course) {
+    return {type: types.UPDATE_COURSE_SUCCESS, course };
 }
 
 export function loadCourses() {
@@ -28,11 +28,12 @@ export function loadCourses() {
 export function saveCourse(course) {
     return function(dispatch, getState) {
         dispatch(beginAjaxCall());
-        return courseApi.saveCourse(course).then(savedCourse => {
-            course.id ? dispatch(updateCoursesSuccess(savedCourse)) :
-                dispatch(createCoursesSuccess(savedCourse));
+        return courseApi.saveCourse(course).then(course => {
+            course.id ? dispatch(updateCourseSuccess(course)) :
+                dispatch(createCourseSuccess(course));
         }).catch(error => {
             throw(error);
         });
     };
 }
+
